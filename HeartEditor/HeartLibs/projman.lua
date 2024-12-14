9af857171deebb6f7eab93135198552c53abbf0e
EggToast <53361644+EggToaster@users.noreply.github.com>
2024-12-14 21:43:37 +0900

    Started to make actual functionality in Projman

--- HeartEditor/HeartLibs/projman.lua
+++ HeartEditor/HeartLibs/projman.lua
@@ -1,10 +1,25 @@
 local projman = {
     _version = "0.0.1",
-    load = function(self, project, path) end,
-    save = function(self, project, path) end,
-    validate = function(self, project, path)
-        local projectfile = io.open(project< "r")
+    load = function(self, projectfilepath, projectpath)
+        if not self:validate(projectfilepath,projectpath) then
+            return false, "he.project.corrupted"
+        end
+
+        local project = {}
+
+
+        return project
+
+    end,
+    save = function(self, projectfilepath, projectpath) end,
+    validate = function(self, projectfilepath, projectpath)
+        local projectfile = io.open(projectfilepath< "r")
         local project = projectfile:read("a")
+        projectfile:close()
+        
+        if not he.json.check(project) then
+            return false
+        end
     end
     
 }
