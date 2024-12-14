2807220a77a0c0b64e2a8364f942f4f96b3660e7
EggToast <53361644+EggToaster@users.noreply.github.com>
2024-12-14 19:35:48 +0900

    Added basics of projman

--- HeartEditor/HeartLibs/projman.lua
+++ HeartEditor/HeartLibs/projman.lua
@@ -1,5 +1,12 @@
 local projman = {
-    test = "aa"
+    _version = "0.0.1",
+    load = function(self, project, path) end,
+    save = function(self, project, path) end,
+    validate = function(self, project, path)
+        local projectfile = io.open(project< "r")
+        local project = projectfile:read("a")
+    end
+    
 }
 
 local name = "project"
