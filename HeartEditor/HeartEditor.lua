2a33d9fd6429c4ede0b8c80c494b8b944874ef8f
EggToast <53361644+EggToaster@users.noreply.github.com>
2024-12-21 15:51:49 +0900

    Improved use of tables to be easier to edit, added WORK(do if you want to release the program)

--- HeartEditor/HeartEditor.lua
+++ HeartEditor/HeartEditor.lua
@@ -1,18 +1,18 @@
-local he = {}
-
-he.info = {
-    version = "0.0.1",
-    channel = "Development",
-    name = "HeartEditor"
+local he = {
+    _quit = false, _quitpopup = false,
+    info = {
+        version = "0.0.1-1", -- WORK: Be sure to change this after beta/normal release. -n means beta release n
+        channel = nil,
+        name = "HeartEditor"
+    },
+    libs = {},
+    enum = {
+        channel = {development = "Development", beta = "Beta", release = "Release"},
+        arg = {verbose = {"-v", "-verbose", "--verbose"}, debug = {"-d", "--debug", "-debug"},
+        hotswap = {"-h", "--hotswap", "-hotswap"}, forcedevelopment = {"-force-dev-channel", "--force-dev-channel"}}
+    }
 }
 
--- Is quitting posible?
-he._quit = false
-he._quitpopup = false
-
-he.enum = {
-    channel = {development = "Development",beta = "Beta",release = "Release"},
-    arg = {verbose = {"-v", "-verbose", "--verbose"}, debug = {"-d", "--debug", "-debug"}, hotswap = {"-h", "--hotswap", "-hotswap"}, forcedevelopment = {"-force-dev-channel", "--force-dev-channel"}}
-}
+he.info.channel = he.enum.channel.development -- WORK: Change this every beta/normal release
 
 return he
